48a064274577d3484f94bb6c9a9b9954ebe0166c
Paulo Cesar <paulocesar.araujo.ads@gmail.com>
2020-07-14 19:30:26 -0300

    Parametrização no LoadView

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -32,12 +33,15 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onmenuItemDepartamentoAction() {
-		loadView2("/gui/ListaDepartamento.fxml");
+		loadView("/gui/ListaDepartamento.fxml", (ListaDepartamentoController controlador) -> {
+			controlador.setDepartamentoServico(new DepartamentoServico());
+			controlador.updateTableView();
+		});
 	}
 	
 	@FXML
 	public void onmenuItemSobreAction() {
-		loadView("/gui/Sobre.fxml");
+		loadView("/gui/Sobre.fxml", X -> {});
 	}
 	
 	@Override
@@ -46,7 +50,7 @@ public class MainViewController implements Initializable {
 		
 	}
 	
-	private void loadView(String absoluteName)  {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> iniAction)  {
 		try{
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 		VBox newVBox = loader.load();
@@ -57,6 +61,9 @@ public class MainViewController implements Initializable {
 		mainVbox.getChildren().clear();
 		mainVbox.getChildren().add(mainMenu);
 		mainVbox.getChildren().addAll(newVBox.getChildren());
+		
+		T controller = loader.getController();
+		iniAction.accept(controller);
 		
 	} catch (IOException e) {
 		Alerts.showAlert("Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
