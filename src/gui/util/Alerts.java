e2fdd08f9ebb51ec080b8737ce0a651ef85aeeed
Paulo Cesar <paulocesar.araujo.ads@gmail.com>
2020-07-14 09:38:40 -0300

    Teste de commit pelo Eclipse

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Alert.AlertType;
 
 public class Alerts {
 
+//	Metodo para alertas
 	public static void showAlert(String title, String header, String content, AlertType type) {
 		Alert alert = new Alert(type);
 		alert.setTitle(title);
